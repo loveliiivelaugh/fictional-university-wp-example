446e4201123d518d2f3ff4f084e7541cea34eaab
Michael Woodward <44563439+MichaelWoodward90@users.noreply.github.com>
2021-01-03 23:13:31 -0600

    initial commit

---
+++ functions.php
@@ -0,0 +1,142 @@
+<?php
+
+function page_banner($args = NULL) {
+
+  if (!$args['title']) {
+    $args['title'] = get_the_title();
+  }
+
+  if (!$args['subtitle']) {
+    $args['subtitle'] = get_field('page_banner_subtitle');
+  }
+
+  if (!$args['photo']) {
+    if (get_field('page_banner_background_image')) {
+    $args['photo'] = get_field('page_banner_background_image')['sizes']['pageBanner'];
+    } else {
+      $args['photo'] = get_theme_file_uri('/images/ocean.jpg');
+    }
+
+  }
+  ?>
+
+    <div class="page-banner">
+      <div 
+        class="page-banner__bg-image" 
+        style="background-image: url(
+          <?php 
+            echo $args['photo'];
+          ?>
+        );">
+      </div>
+        <div class="page-banner__content container container--narrow">
+          <h1 class="page-banner__title"><?php echo $args['title'] ?></h1>
+          <div class="page-banner__intro">
+            <p><?php echo $args['subtitle'] ?></p>
+          </div>
+        </div>  
+    </div>
+
+  <?php
+}
+
+function university_files() {
+  wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+  wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+
+  wp_enqueue_script('googleMap', '//maps.googleapis.com/maps/api/js?key=AIzaSyBvzp4fNX4NjCApNHjnQFMWBiy43GTQh1o', NULL, '1.0', true);
+
+  if (strstr($_SERVER['SERVER_NAME'], 'fictional-university.local')) {
+    wp_enqueue_script('main-university-js', 'http://localhost:3000/bundled.js', NULL, '1.0', true);
+  } else {
+    wp_enqueue_script('our-vendors-js', get_theme_file_uri('/bundled-assets/vendors~scripts.9678b4003190d41dd438.js'), NULL, '1.0', true);
+    wp_enqueue_script('main-university-js', get_theme_file_uri('/bundled-assets/scripts.95196418146a458a7aef.js'), NULL, '1.0', true);
+    wp_enqueue_style('our-main-styles', get_theme_file_uri('/bundled-assets/styles.95196418146a458a7aef.css'));
+  }
+
+  wp_localize_script('main-university-js', 'universityData', array(
+    'root_url' => get_site_url()
+  ));
+
+}
+
+add_action('wp_enqueue_scripts', 'university_files');
+
+function university_features() {
+  register_nav_menu('headerMenuLocation', 'Header Menu Location');
+  register_nav_menu('footerLocationOne', 'Footer Location One');
+  register_nav_menu('footerLocationTwo', 'Footer Location Two');
+  add_theme_support('title-tag');
+  add_theme_support('post-thumbnails');
+  add_image_size('professorLandscape', 400, 260, true);
+  add_image_size('professorPortrait', 480, 650, true);
+  add_image_size('pageBanner', 1500, 350, true);
+}
+
+add_action('after_setup_theme', 'university_features');
+
+
+// Following function modifies the amount of custom event posts are shown on the /events page
+// Event Archive ordered by the nearest approaching event as well as not show 'past events'
+function university_adjust_queries($query) {
+  if (!is_admin() AND is_post_type_archive('campus') AND is_main_query()) {
+    $query->set('posts_per_page', -1);
+  }
+  
+  if (!is_admin() AND is_post_type_archive('program') AND is_main_query()) {
+    $query->set('orderby', 'title');
+    $query->set('order', 'ASC');
+    $query->set('posts_per_page', -1);
+  }
+  
+  if (!is_admin() AND is_post_type_archive('event') AND $query->is_main_query()) {
+    $today = date('Ymd');
+    $query->set('meta_key', 'event_date');
+    $query->set('orderby', 'meta_value_num');
+    $query->set('order', 'ASC');
+    $query->set('meta_query', array(
+      array(
+        'key' => 'event_date',
+        'compare' => '>=',
+        'value' => $today,
+        'type' => 'numeric'
+      )
+    ));
+  }
+}
+
+add_action('pre_get_posts', 'university_adjust_queries');
+
+function universityMapKey($api) {
+  $api['key'] = 'AIzaSyBvzp4fNX4NjCApNHjnQFMWBiy43GTQh1o';
+  return $api;
+}
+
+add_action('rest_api_init', 'register_rest_images' );
+function register_rest_images(){
+    register_rest_field( array('post'),
+        'fimg_url',
+        array(
+            'get_callback'    => 'get_rest_featured_image',
+            'update_callback' => null,
+            'schema'          => null,
+        )
+    );
+}
+function get_rest_featured_image( $object, $field_name, $request ) {
+    if( $object['featured_media'] ){
+        $img = wp_get_attachment_image_src( $object['featured_media'], 'app-thumb' );
+        return $img[0];
+    }
+    return false;
+}
+
+
+add_filter('acf/fields/google_map/api', 'universityMapKey');
+
+add_filter('ai1wm_exclude_content_from_export', 'ignoreCertainFiles');
+
+function ignoreCertainFiles($exclude_filters) {
+  $exclude_filters[] = 'themes/fictional-university-theme/node_modules';
+  return $exclude_filters;
+}
